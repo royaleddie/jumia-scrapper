863d81721bca7f720c3ab5f0b63912db767c7579
royaleddie <iginuaendurance@gmail.com>
2022-05-15 00:56:12 +0100

    jumia scrapper updated

--- jumia/spiders/jumia_bot.py
+++ jumia/spiders/jumia_bot.py
@@ -7,13 +7,13 @@ class JumiaBotSpider(scrapy.Spider):
     start_urls = ['https://www.jumia.com.ng/catalog/?q=oraimo']
 
     def parse(self, response):
-        for products in response.css('article.c-prd'):
+        for products in response.css('a.core'):
             yield {
-                'product_url': products.css('a.core').attrib['href'],
+                # 'product_url': 'https://jumia.com.ng'+products.attrib['href'],
                 'image_url': products.css('img.img').attrib['data-src'],
                 'product_name': products.css('h3.name::text').get(),
-                'product_price': products.css('div.prc::text').get().replace('₦', ''),
-                'rating_count': products.css('div.rev::text').get(),  
+                'product_price': products.css('div.prc::text').get().replace('₦', '').strip() if '₦' in products.css('div.prc::text').get() else products.css('div.prc::text').get(),
+                'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '') if products.css('div.rev::text').get() else 0, 
             }
         next_page = response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]') 
         if next_page and next_page.attrib['href']: 
