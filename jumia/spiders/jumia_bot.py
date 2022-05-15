1d5a146e1cf4cd22d1ce840de8fb7fe33c365828
royaleddie <iginuaendurance@gmail.com>
2022-05-15 01:09:58 +0100

    jumia scrapper updated

--- jumia/spiders/jumia_bot.py
+++ jumia/spiders/jumia_bot.py
@@ -8,13 +8,14 @@ class JumiaBotSpider(scrapy.Spider):
 
     def parse(self, response):
         for products in response.css('a.core'):
-            yield {
-                # 'product_url': 'https://jumia.com.ng'+products.attrib['href'],
-                'image_url': products.css('img.img').attrib['data-src'],
-                'product_name': products.css('h3.name::text').get(),
-                'product_price': products.css('div.prc::text').get().replace('₦', '').strip() if '₦' in products.css('div.prc::text').get() else products.css('div.prc::text').get(),
-                'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '') if products.css('div.rev::text').get() else 0, 
-            }
+            # if(products.css('h3.name::text').get() and products.css('div.prc::text').get()):
+                yield {
+                    'product_url': 'https://jumia.com.ng'+products.attrib['href'],
+                    'image_url': products.css('img.img').attrib['data-src'],
+                    'product_name': products.css('h3.name::text').get(),
+                    'product_price': products.css('div.prc::text').get().replace('₦', '').strip() if '₦' in products.css('div.prc::text').get() else products.css('div.prc::text').get(),
+                    'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '') if products.css('div.rev::text').get() else 0, 
+                }
         next_page = response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]') 
         if next_page and next_page.attrib['href']: 
             next_page_url = 'https://jumia.com.ng'+next_page.attrib['href'] 
