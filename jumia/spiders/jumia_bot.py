f1bec5c3659765ed32040937e34f54be65be027c
royaleddie <iginuaendurance@gmail.com>
2022-05-15 09:26:42 +0100

    jumia scrapper updated

--- jumia/spiders/jumia_bot.py
+++ jumia/spiders/jumia_bot.py
@@ -14,7 +14,7 @@ class JumiaBotSpider(scrapy.Spider):
                     'image_url': products.css('img.img').attrib['data-src'],
                     'product_name': products.css('h3.name::text').get(),
                     'product_price': products.css('div.prc::text').get().replace('₦', '').strip() if '₦' in products.css('div.prc::text').get() else products.css('div.prc::text').get(),
-                    'product_rating': products.css('div.rev::text').get()
+                    'product_rating': products.css('div.rev::text').get().replace('(', '').replace(')', '') if products.css('div.rev::text').get() else 0,
                 }
         next_page = response.xpath('//*[@id="jm"]/main/div[2]/div[3]/section/div[2]/a[6]') 
         if next_page and next_page.attrib['href']: 
